fac29c8ac1331e520d07fd765f8f04271e8f2523
Lorenco <lorenco.zambiazi@gmail.com>
2025-03-18 22:58:38 -0300

    Adicionando Desafio conta banco terminal

---
+++ desafio-banco-terminal/ContaBanco/src/br/com/lorxr/ContaBanco.java
@@ -0,0 +1,52 @@
+package br.com.lorxr;
+
+public class ContaBanco {
+
+    private int numero = 0;
+    private String agencia = "";
+    private String nomeCliente = "";
+    private double saldo = 0.0;
+
+    public ContaBanco() {
+    }
+
+    public ContaBanco(int numero, String agencia, String nomeCliente, double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+}
